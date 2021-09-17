c7afc45853222e2e4cbc5b6e73b5b3a4a0d00d3d
cuongtien00 <cuonghiep000@gmail.com>
2021-09-17 11:01:32 +0700

    them tim UCLN

---
+++ src/UCLN.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class UCLN {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        a = input.nextInt();
+        System.out.println("Nhap b: ");
+        b = input.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 && b == 0) {
+            System.out.println("Khong co UCLN");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                a = b - a;
+        }
+
+        System.out.println("UCLN la : " + a);
+    }
+}
